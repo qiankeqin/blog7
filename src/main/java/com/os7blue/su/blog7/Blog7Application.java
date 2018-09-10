257116b7c10249e1517df5c65dd389b32126305e
os7blue <os7blue@163.com>
2018-09-10 19:28:50 +0800

    "project into"

---
+++ src/main/java/com/os7blue/su/blog7/Blog7Application.java
@@ -0,0 +1,12 @@
+package com.os7blue.su.blog7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blog7Application.class, args);
+    }
+}
